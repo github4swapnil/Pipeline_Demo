28d58fecb0ac9f862f0f3358f6fee03241d5813f
github4swapnil <65334233+github4swapnil@users.noreply.github.com>
2020-05-28 19:24:58 +0530

    Add files via upload

---
+++ Create_Table.sql
@@ -0,0 +1,18 @@
+USE [QACOP]
+GO
+
+/****** Object:  Table [dbo].[Git_Project]    Script Date: 5/14/2020 3:08:45 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Git_Project](
+	[Member_ID   ] [varchar](50) NULL,
+	[Chronic_Condition_Name     ] [varchar](50) NULL,
+	[CC_Service_Date] [varchar](50) NULL,
+) ON [PRIMARY]
+GO
+
+
